0a6a80e229299d83073d250950580e538f11524b
per2 <carol10051113@gmail.com>
2022-10-11 10:49:05 -0700

    new

--- app/src/main/java/com/example/quizapp2022/MainActivity.kt
+++ app/src/main/java/com/example/quizapp2022/MainActivity.kt
@@ -28,30 +28,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadQuestions()
         wireWidgets()
         //get the first question, set up the textviews
         val scoreText = getString(R.string.main_score)
         textView_score.text = "SCore: $<quiz.score"
-        trueButton.setOnClickListener{
-            solution = true
-        }
-
-        falseButton.setOnClickListener {
-            solution = false
-        }
-
-    }
-
-    private fun wireWidgets() {
-        trueButton = findViewById(R.id.button_main_true)
-        falseButton = findViewById(R.id.button_main_false)
-        question = findViewById(R.id.textView_main_question)
-        textView_score = findViewById(R.id.textView_main_score)
-    }
-
-    private fun loadQuestions() {
-        /*
         val inputStream = resources.openRawResource(R.raw.questions)
         val jsonString = inputStream.bufferedReader().use{
             it.readText()
@@ -61,15 +41,44 @@ class MainActivity : AppCompatActivity() {
         val gson = Gson()
 
         val type = object : TypeToken<List<Question>>() { }.type
-        val questions = gson.fromJson<List<Question>>(jsonString, type)
+        val questionsList = gson.fromJson<List<Question>>(jsonString, type)
 
-        //create the quiz object from the list of questions
-        quiz = Quiz(questions)*/
+        Log.d(TAG, "list: $questionList")
+        Log.d(TAG, "test")
+
+        nextQuestion()
+
+        trueButton.setOnClickListener{
+            solution = true
+        }
+
+        falseButton.setOnClickListener {
+            solution = false
+        }
+        if(solution == questionsList.get(count).answer)
+        {
+            score++
+        }
+    }
+
+    private fun wireWidgets() {
+        trueButton = findViewById(R.id.button_main_true)
+        falseButton = findViewById(R.id.button_main_false)
+        question = findViewById(R.id.textView_main_question)
+        textView_score = findViewById(R.id.textView_main_score)
+    }
+
+    private fun nextQuestion() {
         if(count < questionList.size)
         {
-            question.text = questionList(count).getQuestion()
-
+            question.text = questionList.get(count).question
+            count++
         }
+    }
+
+    private fun end()
+    {
+
     }
 
 
